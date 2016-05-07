bc3dd720da57d5aecb0a1a070392b5224349ceeb
mashybuttons <katherine.sj@gmail.com>
2016-05-06 19:45:45 -0700

    Working on live search

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,26 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: { 
+      part: 'snippet',
+      q: options.query,
+      key: options.key,
+      maxResults: options.max,
+      type: 'video',
+
+    },
+    success: function(data) {
+      callback(data.items);
+    },
+    failure: function(data) {
+      console.log('Kat is being mean again...');
+    }
+  });
+
+  // $.get('https://www.googleapis.com/youtube/v3/search', function sucess(data) {
+  //   console.log(data);
+  // });
 };
 
 window.searchYouTube = searchYouTube;
