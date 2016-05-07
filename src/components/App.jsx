9febd196e45e70f91e6a8b79d8271d473ca9bc71
mashybuttons <katherine.sj@gmail.com>
2016-05-06 21:13:15 -0700

    trying to render youtube videos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,8 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      firstVideo: exampleVideoData[0]
+      firstVideo: exampleVideoData[0],
+      videoList: exampleVideoData
     };
   }
 
@@ -13,6 +14,21 @@ class App extends React.Component {
 
   }
 
+  componentDidMount () {
+    console.log('got called');
+    searchYouTube({key: window.YOUTUBE_API_KEY, query: 'cats', max: 10 }, videos => {
+      console.log(videos);
+      console.log(this.setState);
+      this.setState({
+
+        firstVideo: videos[0],
+        videoList: videos
+      });
+    });
+
+    
+  }
+
   render() {
     return (
       <div>
@@ -21,7 +37,7 @@ class App extends React.Component {
           <VideoPlayer video={this.state.firstVideo}/>
         </div>
         <div className="col-md-5">
-          <VideoList clicked={this.onClickEvent.bind(this)} videos={exampleVideoData}/>
+          <VideoList clicked={this.onClickEvent.bind(this)} videos={this.state.videoList}/>
         </div>
       </div>
     );
